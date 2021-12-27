1c34c6be36b55789ad862e002d1531e30ddad149
Rendezvoused <89423721+Rendezvoused@users.noreply.github.com>
2021-12-26 21:44:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+<script src="https://static.tumblr.com/iuw14ew/VSQma1786/jquery.style-my-tooltips.js"></script>
+<script>
+(function($){
+$(document).ready(function() {
+$("a[title]").style_my_tooltips({
+tip_follows_cursor:false,
+tip_delay_time:90,
+tip_fade_speed:600,
+attribute:"title"
+});
+});
+})(jQuery);
+</script>
+
+<script src="https://code.iconify.design/2/2.0.4/iconify.min.js"></script>
+<script src="//pull.cappuccicons.com/cpf.js"></script>
+<script src="https://kit.fontawesome.com/18ffc3f01a.js" crossorigin="anonymous"></script>
+
+<link href="https://static.tumblr.com/qudkd6d/OcDnl99gb/style.css" rel="stylesheet" type="text/css"/>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script src="https://static.tumblr.com/qudkd6d/Az6nkemqr/pxuphotoset.min.js"></script>
